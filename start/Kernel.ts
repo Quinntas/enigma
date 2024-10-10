9342d3e0c16fd869a8a5dd15202972127e02f00f
Caio "Quintas_" Santiago <caioquintassantiago@gmail.com>
2024-10-10 14:02:02 -0300

    Update Kernel.ts

--- start/Kernel.ts
+++ start/Kernel.ts
@@ -5,6 +5,6 @@ import {Server} from "bun";
 enigma.use('/api', router);
 
 enigma.listen((server: Server) => {
-    console.log(`Server is running on http://${server.hostname}:${server.port}`);
+    console.log(`Server is running on ${server.hostname}:${server.port}`);
     server.development && console.log('Development mode is enabled');
 })
