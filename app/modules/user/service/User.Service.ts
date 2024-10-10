84f35f9e4e714bb828a2f4834e31a94f54dfbfca
quintas <caioquintassantiago@gmail.com>
2024-10-10 19:40:12 -0300

    add cryptography

--- app/modules/user/service/User.Service.ts
+++ app/modules/user/service/User.Service.ts
@@ -1,5 +1,7 @@
 import {UserRepository} from "../repository/User.Repository";
 import {Logger} from "../../../../bin/Logger";
+import {Cryptography} from "../../../../utils/crypto";
+import env from "../../../../start/Env";
 
 export class UserService {
     constructor(
@@ -12,9 +14,11 @@ export class UserService {
         email: string,
         password: string
     ) {
+        const hashedPassword = Cryptography.encrypt(password, env.PEPPER)
+
         await this.userRepository.create({
             email,
-            password
+            password: hashedPassword
         })
     }
 }
--- start/Env.ts
+++ start/Env.ts
@@ -3,4 +3,5 @@ import {Env} from "../lib/core/Env";
 export default Env.create('./', {
     PORT: Env.schema.number(),
     POSTGRES_URL: Env.schema.string(),
+    PEPPER: Env.schema.string(),
 })
